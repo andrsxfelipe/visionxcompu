ec6178a0e34ff6f9c4ecb359d905e425ab415c9a
andrsxfelipe <afelipelondono81@gmail.com>
2024-02-01 11:29:31 -0500

    Ejercicio 2 agregado

--- ej1.py
+++ ej1.py
@@ -9,19 +9,36 @@ def show_im(im):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-for i in range(19,80):
-    for j in range(19,80):
-        im[i,j] = (0,0,255)
+def cuadrados():
+    for i in range(19,80):
+        for j in range(19,80):
+            im[i,j] = (0,0,255)
 
-for i in range(29,70):
-    for j in range(29,70):
-        im[i,j] = (0,255,0)
+    for i in range(29,70):
+        for j in range(29,70):
+            im[i,j] = (0,255,0)
 
-for i in range(39,60):
-    for j in range(39,60):
-        im[i,j] = (255,0,0)
-for i in range(44,55):
-    for j in range(44,55):
-        im[i,j] = (0,0,0)        
-          
+    for i in range(39,60):
+        for j in range(39,60):
+            im[i,j] = (255,0,0)
+    for i in range(44,55):
+        for j in range(44,55):
+            im[i,j] = (0,0,0)
+
+#--
+for i in range(0,100,3):
+
+    im[5,i] = (255,0,0)
+for i in range(1,100,5):
+    im[6,i] = (255,0,0)
+for i in range(2,100,5):
+   im[6,i] = (255,0,0)
+for i in range(4,100,5):
+    im[4,i] = (255,0,0)
+for i in range(5,100,5):
+   im[4,i] = (255,0,0)
+
+#--
+
+#cuadrados()           
 show_im(im)
